140186e96cc35737b21419c77c13bb1d8d24c185
Tabassum-coder <tabuj2706@gmail.com>
2025-01-04 17:00:04 +0530

    number guess game

--- activity1.py
+++ activity1.py
@@ -1,30 +1,34 @@
-#Write a Python program to create a Python list of your favourite fruits (minimum 5). 
-# Then perform the given operations on it.
+#Write a Python program to create a Python list of your favourite fruits (minimum 5). Then perform the given operations on it
 
-fruits=["Mango","Apple","Cherry","Banana","WaterMelon"]
+list1=['orange','banana','apple','cherry','kiwi']
 
-#length of list
-print("Length of fruits list is ",len(fruits))
+print("List elements are ",list1)
 
 #indexing
-print("I like ",fruits[4])
-
-#adding values to list
-fruits.append("Kiwi")
-
-print("Updated list is ",fruits)
-
-#removing values from list
-fruits.remove("Banana")
-
-print("Updated list after removing ",fruits)
-
-#sorting
-fruits.sort()
-print("Sorted list is ",fruits)
-
-fruits.reverse()
-print("Reversed list is ",fruits)
+print(list1[2]," is rich in iron ")
 
 #slicing
-print("Sliced List ",fruits[1:3])
+print("Sliced list is ",list1[1:4])
+
+#append
+list1.append('watermelon')
+print("Updated list after adding is ",list1)
+
+list1.sort()
+print("Sorted list is ",list1)
+
+list1.reverse()
+print("Reversed list is ",list1)
+
+#remove
+list1.remove('cherry')
+print("Updated list after removing is ",list1)
+
+#pop
+list1.pop(3)
+print("List after popping an element is ",list1)
+
+print("Length of the list is ",len(list1))
+
+list1.clear()
+print(list1)
